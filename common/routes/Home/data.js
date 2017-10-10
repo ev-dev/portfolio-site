8656858d06c567abd277097e754cc9f90aa316ac
ev-yak@yakketyServ <conspectus.io@gmail.com>
2017-10-10 20:24:47 +0000

    PM2 updates

--- common/routes/Home/data.js
+++ common/routes/Home/data.js
@@ -1,7 +1,7 @@
 const data = [
   {
     resource: 'ExampleJS',
-    link: 'http://examplejs.nvizn.io',
+    link: 'http://examplejs.ev-dev.com',
     description: "This project is motivated by my constant and ever-growing desire for productivity tools. As JavaScript developers learn new techniques and new libraries, their minds can quickly become saturated with ideas from the latest buzzword. When they need a refresher on a certain concept, they turn to (Google->StackOverflow->MDN->Random Blog) but that process is often way too much effort for the all-efficient developer. ExampleJS allows a user to search for any topic in JavaScript (concepts/methods/libraries/etc..) and view a number of use cases for the given query. The project integrates APIs of the Mozilla Developer Network (MDN) and StackOverflow (with more to come) and wraps the wonderful information on those sites into a simple and time saving prompt. ExampleJS is a one-stop destination intended to save devs from having to hunt through articles/docs/blogs so they can get back to work."
   },
   {
--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const config = {
   nodeEnv: process.env.NODE_ENV,
   webConcurrency: process.env.WEB_CONCURRENCY || 1,
-  port: process.env.PORT || 80,
+  port: 3001,
   timeout: 29000
 }
 
